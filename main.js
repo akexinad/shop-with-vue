09d2427f345a428a47c2f13f137418f0be400600
danny <danixeka@gmail.com>
2019-02-22 22:10:45 +1100

    fixed image hover and cart function

--- main.js
+++ main.js
@@ -5,20 +5,22 @@ const app = new Vue({
     product: 'Socks',
     selectedVariant: 0,
     link: 'https://www.youtube.com/watch?v=bp_39m9k1AE',
-    inventory: 0,
-    inStock: true,
+    // inventory: 0,
+    // inStock: true,
     onSale: true,
     details: ['80% cotton', '20% polyester', 'XL'],
     variants: [
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/socks-green.jpeg'
+        variantImage: './assets/socks-green.jpeg',
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/socks-blue.jpeg'
+        variantImage: './assets/socks-blue.jpeg',
+        variantQuantity: 0,
       }
     ],
     sizes: ['XS', 'S', 'M', 'XM', 'L', 'XL', 'XXL', 'DAMN SON YOU BIG FOOT!!!'],
@@ -43,6 +45,22 @@ const app = new Vue({
   computed: {
     title() {
       return this.brand + ' ' + this.product
-    }
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSaleInfo() {
+      return this.brand + ' ' + this.product
+    },
+    sale() {
+      if (this.onSale && this.inStock) {
+        return this.brand + ' ' + this.product + ' are on sale!'
+      } else {
+        return this.brand + ' ' + this.product + ' are not on sale.'
+      }
+    },
   }
 })
