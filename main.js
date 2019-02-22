39d8faf8e7228be858443e2f438551993a9fdbdd
danny <danixeka@gmail.com>
2019-02-22 18:32:51 +1100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/socks-green.jpeg',
+    link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+    inStock: true,
+  }
+})
